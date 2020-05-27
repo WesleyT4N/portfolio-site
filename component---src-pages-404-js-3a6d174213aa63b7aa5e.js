d2e22b31c8d16088208d3fa946657ed61d24f26d
Wes T4N <wesleyt1998@gmail.com>
2020-05-26 21:42:33 -0400

    Updates

---
+++ component---src-pages-404-js-3a6d174213aa63b7aa5e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),o=n.n(a),l=n("Bl7J"),r=n("vrFN");t.default=function(){return o.a.createElement(l.a,null,o.a.createElement(r.a,{title:"404: Not found"}),o.a.createElement("h1",null,"404: The page you looked for doesn't exist."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-3a6d174213aa63b7aa5e.js.map
